ab46642dc68ea8f3986b669da3095ab9314c2393
Stefan Miladinović <stefan@miladinovic.me>
2022-08-24 23:50:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".header_moon").onclick = function () {
   document.querySelector("body").classList.toggle("is-black");
 };
+
+document.querySelector(".header_mob-menu").onclick = function () {
+  document.querySelector(".header-menu").classList.toggle("is-active");
+};
